6fa1c1ab873c153c6b22d06d9db08e9ffbc859b7
yarisingu <yarisingutarun@gmail.com>
2023-06-09 19:26:33 +0530

    new project

---
+++ src/main/java/com/tarun/suppermarket/SuppermarketApplication.java
@@ -0,0 +1,13 @@
+package com.tarun.suppermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarun/suppermarket/SuppermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarun.suppermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
